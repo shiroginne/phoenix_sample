1408e742e80d2cf8ff18fb49dc80c447987d53dd
Mac Shifford <shiroginne@gmail.com>
2016-01-14 11:39:21 +0300

    Chapter 5: login, logout, register user.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
@@ -17,15 +18,27 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = Rumbl.User.changeset(%Rumbl.User{}, user_params)
-    
+    changeset = Rumbl.User.registration_changeset(%Rumbl.User{}, user_params)
+
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 end
