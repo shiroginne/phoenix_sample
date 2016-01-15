7d888a1c865d6c2962a46089fefca915423ebb71
Mac Shifford <shiroginne@gmail.com>
2016-01-15 11:48:08 +0300

    Chapter 8: testing

---
+++ test/models/user_test.exs
@@ -0,0 +1,37 @@
+defmodule Rumbl.UserTest do
+  use Rumbl.ModelCase, async: true
+  alias Rumbl.User
+
+  @valid_attrs %{name: "A User", username: "eva", password: "secret"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+
+    refute changeset.valid?
+  end
+
+  test "registration_changeset password must be at least 6 digits long" do
+    attrs = Map.put(@valid_attrs, :password, "no")
+    changeset = User.registration_changeset(%User{}, attrs)
+
+    refute changeset.valid?
+    assert {:password, {"should be at least %{count} character(s)", count: 6}} in changeset.errors
+  end
+
+  test "registration_changeset with valid attributes hases password" do
+    attrs = Map.put(@valid_attrs, :password, "superstrong1")
+    changeset = User.registration_changeset(%User{}, attrs)
+    %{password: pass, password_hash: password_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert password_hash
+    assert Comeonin.Bcrypt.checkpw(pass, password_hash)
+  end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -21,7 +21,7 @@ defmodule Rumbl.User do
   def registration_changeset(model, params) do
     model
     |> changeset(params)
-    |> cast(params, ~w(password))
+    |> cast(params, ~w(password), [])
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash()
   end
